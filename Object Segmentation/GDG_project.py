5f84365b689597e0a439c4c96c09f5758249e3a2
shaaran Lakshminarayanan <34472894+devshaaran@users.noreply.github.com>
2018-03-26 00:26:16 +0530

    modded
    mod

--- Object Segmentation/GDG_project.py
+++ Object Segmentation/GDG_project.py
@@ -1,4 +1,7 @@
 import numpy as np
+import cv2
+
+
 '''
 
 from sklearn.datasets.samples_generator import make_blobs
@@ -44,35 +47,30 @@ plt.show()
 D = ndimage.distance_transform_edt(thresh)
 localMax = peak_local_max(D, indices=False, min_distance=20,labels=thresh)
 
-# perform a connected component analysis on the local peaks,
-# using 8-connectivity, then appy the Watershed algorithm
 markers = ndimage.label(localMax, structure=np.ones((3, 3)))[0]
 labels = watershed(-D, markers, mask=thresh)
 print("[INFO] {} unique segments found".format(len(np.unique(labels)) - 1))
 
 for label in np.unique(labels):
-    # if the label is zero, we are examining the 'background'
-    # so simply ignore it
+    
     if label == 0:
         continue
 
-    # otherwise, allocate memory for the label region and draw
-    # it on the mask
+    
     mask = np.zeros(gray.shape, dtype="uint8")
     mask[labels == label] = 255
 
-    # detect contours in the mask and grab the largest one
+   
     cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
                             cv2.CHAIN_APPROX_SIMPLE)[-2]
     c = max(cnts, key=cv2.contourArea)
 
-    # draw a circle enclosing the object
+    
     ((x, y), r) = cv2.minEnclosingCircle(c)
     #cv2.circle(img, (int(x), int(y)), int(r), (0, 255, 0), 2)
     cv2.putText(img, "#{}".format(label), (int(x) - 10, int(y)),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
-#from skimage.feature import peak_local_max
-#from skimage.morphology import watershed
+
 from scipy import ndimage
 import argparse
 import cv2
@@ -80,7 +78,7 @@ from skimage.feature import peak_local_max
 from skimage.morphology import watershed
 from scipy import ndimage
 '''
-import cv2
+
 
 
 
